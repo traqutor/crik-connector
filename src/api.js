f842705454a8518e4ac1b7193be7e4246011c139
Mariusz Augustyniak <maugustyniak@gmail.com>
2019-11-18 20:19:28 +0100

    Tweaking connector

--- src/api.js
+++ src/api.js
@@ -7,7 +7,6 @@ import { normalizeResource } from './utils/common';
  * @returns {boolean}
  */
 function hasSignedIn() {
-  request.set('Authorization', 'Bearer ==================================================');
   return true;
 }
 
@@ -28,20 +27,26 @@ async function getCapabilitiesForResource(options, resource) {
 }
 
 async function getResourceById(options, id) {
+  const token = sessionStorage.getItem('token');
+  console.log('token',token);
+  console.log('getResourceById');
   const route = `${options.apiRoot}/files/${id}`;
   const method = 'GET';
-  const response = await request(method, route);
-  return normalizeResource(response.body);
+  const response = await request(method, route).set({Authorization: `Bearer ${token}`});
+  console.log('response', response);
+  return normalizeResource(response.body.response);
 }
 
 async function getChildrenForId(options, { id, sortBy = 'name', sortDirection = 'ASC' }) {
+  console.log('getChildrenForId');
   const route = `${options.apiRoot}/files/${id}/children?orderBy=${sortBy}&orderDirection=${sortDirection}`;
   const method = 'GET';
-  const response = await request(method, route);
-  return response.body.items.map(normalizeResource)
+  const response = await request(method, route).set({Authorization: `Bearer ${options.apiToken}`});
+  return response.body.response.items.map(normalizeResource)
 }
 
 async function getParentsForId(options, id, result = []) {
+  console.log('getParentsForId');
   if (!id) {
     return result;
   }
@@ -54,12 +59,14 @@ async function getParentsForId(options, id, result = []) {
 }
 
 async function getBaseResource(options) {
+  console.log('getBaseResource');
   const route = `${options.apiRoot}/files`;
-  const response = await request.get(route);
+  const response = await request.get(route).set({Authorization: `Bearer ${options.apiToken}`});
   return normalizeResource(response.body);
 }
 
 async function getIdForPartPath(options, currId, pathArr) {
+  console.log('getIdForPartPath');
   const resourceChildren = await getChildrenForId(options, { id: currId });
   for (let i = 0; i < resourceChildren.length; i++) {
     const resource = resourceChildren[i];
@@ -76,6 +83,7 @@ async function getIdForPartPath(options, currId, pathArr) {
 }
 
 async function getIdForPath(options, path) {
+  console.log('getIdForPath');
   const resource = await getBaseResource(options);
   const pathArr = path.split('/');
 
@@ -91,16 +99,19 @@ async function getIdForPath(options, path) {
 }
 
 async function getParentIdForResource(options, resource) {
+  console.log('getParentIdForResource');
   return resource.parentId;
 }
 
 async function uploadFileToId({ apiOptions, parentId, file, onProgress }) {
+  console.log('uploadFileToId');
   const route = `${apiOptions.apiRoot}/files`;
-  return request.post(route).
-    field('type', 'file').
-    field('parentId', parentId).
-    attach('files', file.file, file.name).
-    on('progress', event => {
+  return request.post(route)
+      .set({Authorization: `Bearer ${apiOptions.apiToken}`})
+      .field('type', 'file')
+      .field('parentId', parentId)
+      .attach('files', file.file, file.name)
+      .on('progress', event => {
       onProgress(event.percent);
     });
 }
@@ -111,8 +122,9 @@ async function downloadResources({ apiOptions, resources, onProgress }) {
     `${apiOptions.apiRoot}/download?`
   );
 
-  const res = await request.get(downloadUrl).
-    responseType('blob').
+  const res = await request.get(downloadUrl)
+      .set({Authorization: `Bearer ${apiOptions.apiToken}`})
+      .responseType('blob').
     on('progress', event => {
       onProgress(event.percent);
     });
@@ -121,6 +133,7 @@ async function downloadResources({ apiOptions, resources, onProgress }) {
 }
 
 async function createFolder(options, parentId, folderName) {
+  console.log('createFolder');
   const route = `${options.apiRoot}/files`;
   const method = 'POST';
   const params = {
@@ -128,26 +141,36 @@ async function createFolder(options, parentId, folderName) {
     name: folderName,
     type: 'dir'
   };
-  return request(method, route).send(params)
+  return request(method, route)
+      .set({ Authorization: `Bearer ${options.apiToken}` })
+      .set({ 'content-type': 'application/json' })
+      .send(params)
 }
 
 function getResourceName(apiOptions, resource) {
+  console.log('getResourceName');
   return resource.name;
 }
 
 async function renameResource(options, id, newName) {
+  console.log('renameResource');
   const route = `${options.apiRoot}/files/${id}`;
   const method = 'PATCH';
-  return request(method, route).type('application/json').send({ name: newName })
+  return request(method, route)
+      .set({Authorization: `Bearer ${options.apiToken}`})
+      .type('application/json')
+      .send({ name: newName })
 }
 
 async function removeResource(options, resource) {
+  console.log('removeResource');
   const route = `${options.apiRoot}/files/${resource.id}`;
   const method = 'DELETE';
-  return request(method, route)
+  return request(method, route).set({Authorization: `Bearer ${options.apiToken}`})
 }
 
 async function removeResources(options, selectedResources) {
+  console.log('removeResources');
   return Promise.all(selectedResources.map(resource => removeResource(options, resource)))
 }
 
